61744fb9bb08c0a70057d83b16c5daf664c5f260
AnnaKanunnikova <hanna.kanunnikova@mobius-software.com>
2019-02-07 16:27:47 +0200

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 import subprocess
 import os
 
-#Input password for sudo________________!!!IMPORTANT!!!DONT FORGET!!!
-sudoPassword = 'INPUT PASSWORD'
-command = 'pip install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-16.04 wxPython'
-p = os.system('echo %s|sudo -S %s' % (sudoPassword, command))
-
-subprocess.check_call(['pip', 'install', 'SQLAlchemy'])
-subprocess.check_call(['pip', 'install', 'autobahn[twisted]'])
-subprocess.check_call(['pip', 'install', 'numpy'])
-#subprocess.check_call(['pip', 'install', 'pyopenssl or pip install pyOpenSSL==18.0.0'])
-subprocess.check_call(['pip', 'install', 'pyopenssl'])
-subprocess.check_call(['pip', 'install', 'service_identity'])
-subprocess.check_call(['pip', 'install', 'pem'])
-subprocess.check_call(['pip', 'install', 'python3-dtls'])
+subprocess.check_call(['pip3', 'install', '-U', '-f','https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-16.04', 'wxPython'])
+subprocess.check_call(['pip3', 'install', 'SQLAlchemy'])
+subprocess.check_call(['pip3', 'install', 'autobahn[twisted]'])
+subprocess.check_call(['pip3', 'install', 'numpy'])
+subprocess.check_call(['pip3', 'install', 'cryptography'])
+subprocess.check_call(['pip3', 'install', 'pyOpenSSL'])
+subprocess.check_call(['pip3', 'install', 'service_identity'])
+subprocess.check_call(['pip3', 'install', 'pem'])
+subprocess.check_call(['pip3', 'install', 'python3-dtls'])
 
 
