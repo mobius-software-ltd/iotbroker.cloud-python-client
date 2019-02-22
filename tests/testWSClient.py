cba4e908c64cd5ee9fbb79b6a38e349fb332c81e
Vladimir Baranov <vladimir.baranov@mobius-software.com>
2019-02-22 15:09:41 +0200

    clear credentials

--- tests/testWSClient.py
+++ tests/testWSClient.py
@@ -5,7 +5,7 @@ from autobahn.twisted.websocket import connectWS
 
 url = 'ws://broker.iotbroker.cloud:18080/ws'
 
-data = json.dumps({"packet":1,"protocolLevel":4,"username":"yulian.oifa@mobius-software.com","password":"galina1988","clientID":"client1","cleanSession":True,"keepalive":30,"will":None,"willFlag":False,"passwordFlag":True,"usernameFlag":True,"protocolName":"MQTT","retain":True,"content":"dsfgdjnksd"})
+data = json.dumps({"packet":1,"protocolLevel":4,"username":"username","password":"password","clientID":"client1","cleanSession":True,"keepalive":30,"will":None,"willFlag":False,"passwordFlag":True,"usernameFlag":True,"protocolName":"MQTT","retain":True,"content":"dsfgdjnksd"})
 print('encoded message= ' + str(data))
 
 factory = WSSocketClientFactory(url, None)
